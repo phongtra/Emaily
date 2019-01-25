04cb76431717c67b067b50b7028e09adaaad2c95
Phongtran1998 <hoang_phong98@me.com>
2019-01-25 21:31:37 +0200

    changed index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
-
+app.get("/hello", (req, res) => {
+  res.send({ hello: "motherfucker" });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
