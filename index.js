7cf4400b45c05642a0846eca4753ffb890cc2b97
Phongtran1998 <hoang_phong98@me.com>
2019-01-27 18:04:06 +0200

    finished auth flow

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
+require("./models/User");
+mongoose.connect(keys.mongoURI);
+require("./services/passport");
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 100,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require("./routes/authRoutes")(app);
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
-app.get("/hello", (req, res) => {
-  res.send({ hello: "motherfucker" });
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
