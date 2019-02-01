7a21ba5034542fda2feafc7da398cc90b4c96704
Phongtran1998 <hoang_phong98@me.com>
2019-02-01 21:02:23 +0200

    Redeploy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   //Express will serve up the production assets
-  app.use(express.static("/client/build"));
+  app.use(express.static(__dirname + "/client/build"));
   //Express will serve up the index.html if
   //it doesn't recognize the route
   const path = require("path");
