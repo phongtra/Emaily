0511d80a5cbd71728649089812c51c6e26a70b69
Phongtran1998 <hoang_phong98@me.com>
2019-02-10 20:26:09 +0200

    users can see survey result

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -2,26 +2,23 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../actions";
 const renderSurveys = props => {
-  return (
-    props.survey.length &&
-    props.survey.reverse().map(survey => {
-      return (
-        <div className="card red accent-2" key={survey._id}>
-          <div className="card-content white-text">
-            <span className="card-title">{survey.title}</span>
-            <p>{survey.body}</p>
-            <p className="right">
-              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
-            </p>
-          </div>
-          <div className="card-action white-text">
-            <a>Yes: {survey.yes}</a>
-            <a>No: {survey.no}</a>
-          </div>
+  return props.survey.reverse().map(survey => {
+    return (
+      <div className="card red accent-2" key={survey._id}>
+        <div className="card-content white-text">
+          <span className="card-title">{survey.title}</span>
+          <p>{survey.body}</p>
+          <p className="right">
+            Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+          </p>
         </div>
-      );
-    })
-  );
+        <div className="card-action white-text">
+          <a>Yes: {survey.yes}</a>
+          <a>No: {survey.no}</a>
+        </div>
+      </div>
+    );
+  });
 };
 const SurveyList = props => {
   useEffect(() => {
