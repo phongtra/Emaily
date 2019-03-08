9963b25ed9aaeac698031f293e60a4cac344d322
Phongtran1998 <hoang_phong98@me.com>
2019-03-08 22:42:30 +0200

    Fixed Google oauth

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../actions";
+
 const renderSurveys = props => {
   return props.survey.reverse().map(survey => {
     return (
